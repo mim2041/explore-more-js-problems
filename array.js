f937184d8cf35659c5e5644fe393359ed9b39893
mim2041 <suraiyamim338@gmail.com>
2023-02-02 23:23:15 +0600

    finding the type of array, array slice, array splice

---
+++ array.js
@@ -0,0 +1,30 @@
+const country = 'Bangladesh';
+const age = 52;
+const isIndependent = true;
+const student = {id: 121, class: 11, name: 'Agun'};
+const friends = [13, 14, 11, 17, 21, 16, 15, 20];
+function add(num1, num2){
+    return num1+num2;
+}
+
+console.log(typeof country);
+console.log(typeof age);
+console.log(isIndependent);
+console.log(typeof student);
+console.log(typeof friends);
+// check array using Array.isArray
+console.log(Array.isArray(friends));
+console.log(typeof add);
+
+console.log(friends.includes(19));
+
+if(friends.indexOf(19) !== -1){
+    console.log('exists');
+}
+else{
+    console.log('does not exist');
+}
+
+const newFriendsAge = [12, 13, 11, 13];
+const allFriends = newFriendsAge.concat(friends);
+console.log(allFriends);
